c41aa26859c4436cb5e8d63e37994f08e75e7434
RainMic1019 <1281728272@qq.com>
2023-08-22 21:19:04 +0800

    add main

--- README.md
+++ README.md
@@ -1,2 +1,16 @@
-# MyRobot
-KOOK Robot Test.
+﻿# KOOK 机器人测试代码
+
+## 1 环境搭建
+
+1. 安装 Python >= 3.6.8。
+2. 安装 khl.py。
+
+```bash
+pip install khl.py
+```
+
+## 2 运行脚本
+
+```bash
+python .\main.py
+```
---
+++ main.py
@@ -0,0 +1,12 @@
+﻿from khl import *
+from random import randint
+
+bot = Bot(token='1/MjI3NTE=/fjEPJ0zRImyYGvhdb/9eCw==')
+
+
+@bot.command(name='投骰子')
+async def rollDice(msg: Message):
+    randNumber = randint(1, 6)
+    await msg.reply('您掷出的点数是：' + str(randNumber))
+
+bot.run()
